3450a68fe9626f632481f431b437b966bea9418e
James Harris <computersherpa@gmail.com>
2020-10-15 01:02:56 -0500

    Process reactions

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 // const { prefix } = require('./config.json');
 
+const messageExpirationTime = 1000 * 60 * 5; // 5 minutes
+
 client.once('ready', () => {
 	console.log("Starting gallery-bot");
 });
@@ -10,14 +12,27 @@ client.login(process.env.BOT_TOKEN);
 
 client.on('message', message => {
 	message.attachments.each(attachment => {
-		console.log("Name: ", attachment.name)
-		message.channel.send(`Thanks for sharing your work! Would you like your work shown in our gallery?
+		console.log("Detected ", attachment.name)
+		message.channel.send(`Thanks for sharing your work, <@${message.author.id}>! Would you like ${attachment.name} shown in our gallery?
 
-Options: :regional_indicator_a: Always, :thumbsup: Yes please, :thumbsdown: No thank you, :regional_indicator_n: No, and don't ask me again`).then(sentMessage => {
+Options: :regional_indicator_a: Always, :thumbsup: Yes please, :thumbsdown: No thank you, :regional_indicator_n: No, and don't ask me again`)
+		.then(sentMessage => {
 			sentMessage.react('🇦')
 				.then(() => sentMessage.react('👍'))
 				.then(() => sentMessage.react('👎'))
 				.then(() => sentMessage.react('🇳'))
+			;
+			const reactjiFilter = (reaction, user) => {
+				return user.id === message.author.id && ['🇦', '👍', '👎', '🇳'].includes(reaction.emoji.name)
+			}		
+			sentMessage.awaitReactions(reactjiFilter, { max: 1, time: messageExpirationTime, errors: ['time'] })
+				.then(collected => {
+					sentMessage.channel.send("Received " + collected.firstKey());
+					sentMessage.delete();
+				})
+				.catch()
+			;
+			sentMessage.delete({ timeout: messageExpirationTime });
 		})
 	});
 })
