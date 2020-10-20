94e48ff9c06586f499752efbdcf104e91a382524
James Harris <computersherpa@gmail.com>
2020-10-20 00:23:48 -0500

    Basic tag deletion

---
+++ commands/deleteTag.js
@@ -0,0 +1,24 @@
+const { Tags } = require('../database');
+
+module.exports = {
+	name: 'deletetag',
+	description: 'Delete a tag',
+	async execute(message, args) {
+		if (args.length == 0) {
+			message.channel.send("This command deletes a tag. Example: To delete the `doomed` tag, type `!deletetag doomed`");
+		} else {
+			try {
+				const tagName = args[0];
+				const rowCount = await Tags.destroy({ where: { name: tagName }});
+				if (rowCount == 0) {
+					return message.channel.send(`Tag \`${tagName}\` not found.`);
+				} else {
+					return message.channel.send(`Tag \`${tagName}\` deleted.`);
+				}
+			} catch (error) {
+				console.error(error);
+				return message.channel.send("Couldn't delete the \`${tagName}\` tag. Something went wrong.");
+			}
+		}
+	},
+};
